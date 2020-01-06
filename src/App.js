79014a1b8917e7d40431a68117a643f2fa692b3b
xusong-BeiJing <xu.song@rytonh.com>
2020-01-06 22:17:09 +0800

    [建立项目]

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import HelloMyapp from '../src/pages/index'
 
+// state = {
+//   applyNum:'',
+// }
+
+// handleInput = event =>{
+//   let value = event.target.value; 
+// }
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
-        
+      <HelloMyapp />
+        <div>
+            <p>
+              {/* <input type="text" value={this.state.applyNum} placeholder="0.00" onInput={this.handleInput} maxLength="5" /> */}
+            </p>
+        </div>
       </header>
     </div>
   );
---
+++ src/pages/index.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class helloMyApp extends React.Component{
+    // static propTypes = '首页'
+    state={
+        applyNum:''
+    }
+
+    constructor(...args){
+        super(...args);
+        let app = {
+            id:10,
+        }
+    }
+    // 正则验证
+    testInput = val =>{
+        if(/^[A-Za-z0-9]+$/.test(val)) {
+            return val += val
+        }else{
+            return val 
+        }
+    }
+
+    //点击验证
+    handleInput = event =>{
+        let value = event.target.value;
+        let me= this;
+        this.setState({
+            applyNum:me.testInput(value),
+        },()=>{
+            console.log('value',me.state.applyNum)
+        })
+        
+    }
+    render(){
+        return(
+            <div>
+              <input type="text" value={this.state.applyNum} placeholder="开始输入" onChange={this.handleInput} />
+            </div>
+        )
+    }
+ 
+}
+export default helloMyApp;
